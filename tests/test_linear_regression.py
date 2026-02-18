dcafbda5fa7cac6a31a0a1301c8658887618a4b1
sebastian2000pk <juochoas@unal.edu.co>
2026-02-18 08:08:52 -0500

    test: complete test linear regression

--- tests/test_linear_regression.py
+++ tests/test_linear_regression.py
@@ -1,7 +1,32 @@
 from nanoml.linear_model import LinearRegression
 import numpy as np
+import pytest
 
 class TestLinearRegression:
+  def test_initialization(self):
+    model = LinearRegression()
+    assert model.coef_ is None
+    assert model.intercept_ is None
+
+  def test_fit_with_valid_data(self, simple_linear_data: tuple[list, list]):
+    X, y = simple_linear_data
+    model = LinearRegression()
+    model.fit(X, y)
+    assert model.coef_ is not None
+    assert model.intercept_ is not None
+
+  def test_predict_output_shape(self, simple_linear_data: tuple[list, list]):
+    X, y = simple_linear_data
+    model = LinearRegression()
+    model.fit(X, y)
+    predictions = model.predict(X)
+    assert predictions.shape == y.shape
+
+  def test_fit_with_invalid_data(self):
+      model = LinearRegression()
+      with pytest.raises(ValueError):
+        model.fit(None, None)
+
   def test_fit_predict(self):
     X = np.array([[1], [2], [3], [4], [5]])
     y = np.array([2, 4, 6, 8, 10])
@@ -11,6 +36,7 @@ class TestLinearRegression:
     model = LinearRegression()
     model.fit(X, y)
     predictions = model.predict(X)
+
     assert np.allclose(predictions, y), f"Expected {y}, but got {predictions}"
     assert np.allclose(model.coef_, expected_coef), "Coeficiente incorrecto"
     assert np.allclose(model.intercept_, expected_intercept), "Intersección incorrecta" 
