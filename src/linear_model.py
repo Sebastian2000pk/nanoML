77cc7a393863fc20fc989143bf662e3ab6686193
sebastian2000pk <juochoas@unal.edu.co>
2026-02-18 07:14:37 -0500

    predict: regresion linear

--- src/linear_model.py
+++ src/linear_model.py
@@ -1,4 +1,13 @@
 from .base import BaseEstimator
 
 class LinearRegression(BaseEstimator):
+  def __init__(self):
+    self.b0 = 0
+    self.b1 = 0
   
+  def fit(self, X, y):
+    pass
+
+  def predict(self, X):
+    Y = self.b0 + self.b1*X
+    return Y
